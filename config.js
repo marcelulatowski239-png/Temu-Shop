b5fa91326f5e839661b79046007608f280ea55f9
marcelulatowski239-png <marcelulatowski239@gmail.com>
2026-02-28 11:54:34 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
     prefix: "!",
     ticketCategoryId: "1475471754221850857",
     staffRoleId: "1472956273854255331",
-    logChannelId: "1472956274973999213"
+    logChannelId: "1477257514553708617"
 };
